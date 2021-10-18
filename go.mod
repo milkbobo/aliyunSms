d8fe68fa08e6592c587dd1f0088d4e65dd4bb295
Edward <250740918@qq.com>
2021-10-18 10:43:16 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module main
+
+go 1.16
+
+require (
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1290
+	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/smartystreets/assertions v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/dysmsapi"
+)
+
+func main() {
+
+	phoneNumbers := flag.String("phoneNumbers", "", "请输入接受手机号码")
+	templateCode := flag.String("templateCode", "", "请输入模版号码")
+	templateParam := flag.String("templateParam", "", "请输入模版内容")
+	signName := flag.String("signName", "文储", "请输入模版内容")
+
+	flag.Parse()
+	client, err := dysmsapi.NewClientWithAccessKey("ap-northeast-1", "LTAIkTnta6mAZWi6", "yoEErikPWWmBnvCXNKmCOAvQmtBAlP")
+
+	request := dysmsapi.CreateSendSmsRequest()
+	request.Scheme = "https"
+
+	request.SignName = *signName
+	request.PhoneNumbers = *phoneNumbers
+	request.TemplateCode = *templateCode
+	request.TemplateParam = *templateParam
+
+	response, err := client.SendSms(request)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	fmt.Println(response.String())
+}
